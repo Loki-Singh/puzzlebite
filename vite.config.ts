6f795016e212402e2779dc77613c76ff314c8cae
may159ran <mayurrandive159@gmail.com>
2026-01-14 18:07:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// Change "puzzlebite" to your repo name if different
+// // Change "puzzlebite" to your repo name if different
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/puzzlebite/'
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  base: '/puzzlebite/'
-})
+  base: "/",   // MUST be this for S3 root hosting
+});
